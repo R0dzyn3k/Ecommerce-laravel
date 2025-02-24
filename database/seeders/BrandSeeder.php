1b250a8c380785f7337e72c68c65694365049084
R0dzyn3k <Bartosz.kluska@promoznawcy.pl>
2025-02-24 22:16:08 +0100

    feat: add images to seeders

--- database/seeders/BrandSeeder.php
+++ database/seeders/BrandSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 
 use App\Models\Brand;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\File;
 
 
 class BrandSeeder extends Seeder
@@ -19,6 +20,7 @@ class BrandSeeder extends Seeder
                 'title' => 'Bialetti',
                 'description' => 'Włoska marka znana z produkcji klasycznych kawiarek Moka Express, które stały się ikoną włoskiej kawy.',
                 'is_active' => true,
+                'image' => 'bialetti.webp',
             ],
             [
                 'title' => 'Moka',
@@ -29,16 +31,19 @@ class BrandSeeder extends Seeder
                 'title' => 'Hario',
                 'description' => 'Japońska firma produkująca wysokiej jakości sprzęt do parzenia kawy, w tym słynne drippery V60.',
                 'is_active' => true,
+                'image' => 'hario.webp',
             ],
             [
                 'title' => 'AeroPress',
                 'description' => 'Marka, która zrewolucjonizowała świat kawy dzięki swojemu unikalnemu urządzeniu do parzenia.',
                 'is_active' => true,
+                'image' => 'aeropress.webp',
             ],
             [
                 'title' => 'Chemex',
                 'description' => 'Producent eleganckich szklanych zaparzaczy do kawy, które łączą design z funkcjonalnością.',
                 'is_active' => true,
+                'image' => 'chemex.webp',
             ],
             [
                 'title' => 'Lavazza',
@@ -67,12 +72,22 @@ class BrandSeeder extends Seeder
             ],
         ];
 
-        foreach ($brands as $brand) {
-            Brand::create([
-                'title' => $brand['title'],
-                'description' => $brand['description'],
-                'is_active' => $brand['is_active'],
+        foreach ($brands as $brandData) {
+            $brand = Brand::create([
+                'title' => $brandData['title'],
+                'description' => $brandData['description'],
+                'is_active' => $brandData['is_active'],
             ]);
+
+            if (! isset($brandData['image'])) {
+                continue;
+            }
+
+            if (File::exists(public_path('images/brands/' . $brandData['image']))) {
+                $brand->addMedia(public_path('images/brands/' . $brandData['image']))
+                    ->preservingOriginal()
+                    ->toMediaCollection('brand_photo');
+            }
         }
     }
 }
--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 
 use App\Models\Category;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\File;
 
 
 class CategorySeeder extends Seeder
@@ -19,21 +20,25 @@ class CategorySeeder extends Seeder
                 'title' => 'Drippery',
                 'description' => 'Drippery to urządzenia do ręcznego zaparzania kawy metodą przelewową, które pozwalają uzyskać pełny smak i aromat kawy.',
                 'is_active' => true,
+                'image' => 'dripper.webp',
             ],
             [
                 'title' => 'AeroPress',
                 'description' => 'AeroPress to nowoczesne urządzenie do szybkiego i łatwego parzenia kawy o bogatym smaku i niskiej kwasowości.',
                 'is_active' => true,
+                'image' => 'products/aeropress.webp',
             ],
             [
                 'title' => 'Kawiarki',
                 'description' => 'Kawiarki, znane również jako Moka pot, to klasyczne urządzenia do zaparzania kawy w stylu włoskim.',
                 'is_active' => true,
+                'image' => 'kawiarka.webp',
             ],
             [
                 'title' => 'Chemex',
                 'description' => 'Chemex to elegancki zaparzacz do kawy, który pozwala na uzyskanie wyjątkowo czystego smaku naparu dzięki filtrowi papierowemu.',
                 'is_active' => true,
+                'image' => 'products/chemex.webp',
             ],
             [
                 'title' => 'Ekspresy ciśnieniowe',
@@ -44,25 +49,39 @@ class CategorySeeder extends Seeder
                 'title' => 'Kawy mielone',
                 'description' => 'Kawy mielone to wygodne rozwiązanie dla miłośników kawy, którzy chcą cieszyć się smakiem świeżo mielonej kawy bez młynka.',
                 'is_active' => true,
+                'image' => 'products/IllyEspresso.webp',
             ],
             [
                 'title' => 'Kawy ziarniste',
                 'description' => 'Kawy ziarniste to najlepszy wybór dla osób ceniących sobie świeżo mieloną kawę i pełnię smaku.',
                 'is_active' => true,
+                'image' => 'products/lavazza.webp',
             ],
             [
                 'title' => 'Akcesoria do kawy',
                 'description' => 'Wszystko, czego potrzebujesz do przygotowania i podania kawy – od tamperów po filtry papierowe.',
                 'is_active' => true,
+                'image' => 'products/Tamper.webp',
             ],
         ];
 
-        foreach ($categories as $category) {
-            Category::create([
-                'title' => $category['title'],
-                'description' => $category['description'],
-                'is_active' => $category['is_active'],
+        foreach ($categories as $categoryData) {
+            $category = Category::create([
+                'title' => $categoryData['title'],
+                'description' => $categoryData['description'],
+                'is_active' => $categoryData['is_active'],
             ]);
+
+
+            if (! isset($brandData['image'])) {
+                continue;
+            }
+
+            if (File::exists(public_path('images/' . $categoryData['image']))) {
+                $category->addMedia(public_path('images/' . $categoryData['image']))
+                    ->preservingOriginal()
+                    ->toMediaCollection('category_photo');
+            }
         }
     }
 }
